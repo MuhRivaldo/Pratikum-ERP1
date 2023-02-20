5c662694431251d7f8e76c356ca978ff9628ab11
MuhRivaldo <125849108+MuhRivaldo@users.noreply.github.com>
2023-02-20 11:25:36 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "author"        : "Muhammad Rivaldo Sugiyanto",
+    "website"       : "https://www.detik.com/",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+        Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+
+}
